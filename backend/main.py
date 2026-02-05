720deb94db95e480dcfb0488d1d7d980c22d40ad
Maham Gul <mahamgul2324@gmail.com>
2026-02-05 22:57:53 +0500

    Database Connection

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # React frontend
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
